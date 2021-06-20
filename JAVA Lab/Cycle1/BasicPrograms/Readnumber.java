ef2b13760ea8f43ee920cba2406d5b61ca01d74d
19WH1A1259 <69143372+19WH1A1259@users.noreply.github.com>
2021-06-20 22:01:30 +0530

    Add files via upload

---
+++ JAVA Lab/Cycle1/BasicPrograms/Readnumber.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Readnumber
+{
+public static void main(String[] args)
+{
+Scanner num = new Scanner(System.in);
+System.out.println("Enter a number:");
+int a = num.nextInt();
+
+System.out.println("Number: " +a);
+}
+}
