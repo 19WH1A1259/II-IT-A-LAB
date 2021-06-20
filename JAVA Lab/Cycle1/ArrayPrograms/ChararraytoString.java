95df0bed78a3ca977cd42d7a3f2a7f8e2b1f9330
19WH1A1259 <69143372+19WH1A1259@users.noreply.github.com>
2021-06-20 20:59:04 +0530

    Add files via upload

---
+++ JAVA Lab/Cycle1/ArrayPrograms/ChararraytoString.java
@@ -0,0 +1,9 @@
+class ChararraytoString
+{
+public static void main(String[] args)
+{
+char[] ch={'b', 'e', 'a', 'u', 't', 'y'};
+String str=new String(ch);
+System.out.print("Char Array to String: "+str);
+}
+}
